924f002a4ff6984e9402a572b801c5a45e5611ed
Danis <zinnatullin2012@inbox.ru>
2022-05-15 22:31:20 +0300

    initial

---
+++ store/views.py
@@ -0,0 +1,29 @@
+# Create your views here.
+from django.views.generic import ListView, TemplateView
+
+from store.models import Service, Book
+
+
+class ServiceListView(ListView):
+    model = Service
+    context_object_name = 'services'
+    template_name = ''
+
+    def get_queryset(self):
+        return Service.objects.all()
+
+
+class ProductListView(ListView):
+    model = Book
+    context_object_name = 'books'
+    template_name = ''
+
+    def get_queryset(self):
+        return Book.objects.all()
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+from django.shortcuts import render
+
+# Create your views here.
